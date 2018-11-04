84965ed281bb51619997e01d399ceeb8d33cc824
Jisang-Park <jsp13@snu.ac.kr>
2018-11-05 05:50:19 +0900

    Test main component

--- SNU-slam/src/app/main/main.component.spec.ts
+++ SNU-slam/src/app/main/main.component.spec.ts
@@ -1,14 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Router } from "@angular/router";
 import { MainComponent } from './main.component';
 
 describe('MainComponent', () => {
   let component: MainComponent;
   let fixture: ComponentFixture<MainComponent>;
+  let mainComponent: jasmine.SpyObj<MainComponent>;
+  let router: jasmine.SpyObj<Router>;
 
   beforeEach(async(() => {
+    const mainSpy = jasmine.createSpyObj('MainComponent',
+      ['court1', 'court2', 'court3', 'sign_in']);
+
     TestBed.configureTestingModule({
-      declarations: [ MainComponent ]
+      declarations: [ MainComponent ],
+      providers: [ { provide: Router, useValue: router },
+        { provide: MainComponent, useValue: mainSpy }]
     })
     .compileComponents();
   }));
@@ -16,10 +23,29 @@ describe('MainComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(MainComponent);
     component = fixture.componentInstance;
+    mainComponent = TestBed.get(MainComponent);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call court1', () => {
+    expect(mainComponent.court1).toBeTruthy();
+  })
+
+  it('should call court2', () => {
+    expect(mainComponent.court2).toBeTruthy();
+  })
+
+  it('should call court3', () => {
+    expect(mainComponent.court3).toBeTruthy();
+  })
+
+  it('should call sign_in', () =>{
+    mainComponent.sign_in();
+    expect(mainComponent.sign_in).toHaveBeenCalled();
+  })
+
 });
--- SNU-slam/src/app/services/user.service.spec.ts
+++ SNU-slam/src/app/services/user.service.spec.ts
@@ -1,9 +1,35 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject, async } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
 
 import { UserService } from './user.service';
+import { User } from '../user';
+
+const mockUserList: User[] = [
+  { id: 1, email: 'iluvswpp@snu.ac.kr', password: 'q1w2e3r4', nickname: 'Nicolas', position: 'C',
+    wins: 12, loses: 5, teams_id: [1, 2, 5], point: 1984 },
+  { id: 2, email: 'swpp@hate.ac.kr', password: 'sosleepy', nickname: 'OMG', position: 'SG',
+    wins: 7, loses: 21, teams_id: [5], point: 1402 }
+];
+
+const mockUser: User = { id: 3, email: 'idont@like.swpp', password: 'passmetheball', nickname: 'NULL', position: 'SF',
+  wins: 53, loses: 21, teams_id: [3, 4], point: 2130 }
 
 describe('UserService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let userService: UserService;
+  const userApi = '/api/user/';
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [UserService]
+    });
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+    userService = TestBed.get(UserService);
+  });
 
   it('should be created', () => {
     const service: UserService = TestBed.get(UserService);
