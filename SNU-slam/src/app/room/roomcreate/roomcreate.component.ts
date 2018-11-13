ef5f661ee558191cee6d63e60d89035ab1bc7b11
Jisang-Park <jsp13@snu.ac.kr>
2018-11-14 02:27:28 +0900

    Tournament create

--- SNU-slam/src/app/room/roomcreate/roomcreate.component.ts
+++ SNU-slam/src/app/room/roomcreate/roomcreate.component.ts
@@ -28,6 +28,14 @@ export class RoomcreateComponent implements OnInit {
     );
   }
   createroom() {
+    if (this.title.trim().length == 0 || this.location.trim().length == 0 ||
+      this.play_time == null || this.type == null) {
+      return;
+    }
+
+    const check = confirm('Title: ' + this.title + '\n' + 'Location: ' + this.location + '\n' + 'Play time: ' +
+      this.play_time + ' min' + '\n' + 'Type: ' + this.type + ':' + this.type + '\n' + 'Is it correct?');
+
     const newroom = {
       title: this.title,
       host_id: this.id,
@@ -36,8 +44,11 @@ export class RoomcreateComponent implements OnInit {
       play_time: this.play_time,
       type: this.type
     };
-    this.roomService.addRoom( newroom as Room ).subscribe(
-      room => this.router.navigate([`room/${room.id}`])
-    );
+    if (check) {
+      this.roomService.addRoom( newroom as Room ).subscribe(
+        room => this.router.navigate([`room/${room.id}`])
+      );
+    }
+
   }
 }
