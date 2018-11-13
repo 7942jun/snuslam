a6e56858dfc4d5da90733999076c2ad785af5eab
Shim Jaeseon <shimjason@snu.ac.kr>
2018-11-14 01:03:32 +0900

    getUser in auth service

--- SNU-slam/src/app/services/user.service.ts
+++ SNU-slam/src/app/services/user.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from "rxjs";
-import { User } from "../user";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Observable, of } from 'rxjs';
+import { User } from '../user';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { catchError, tap } from 'rxjs/operators';
 
 const httpOptions = {
