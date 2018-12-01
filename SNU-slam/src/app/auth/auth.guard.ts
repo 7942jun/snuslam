7ed5ec5ae7f5dbd94fd50bf0a600a7dd41e4414b
Shim Jaeseon <shimjason@snu.ac.kr>
2018-12-01 22:18:37 +0900

    auth configure

--- SNU-slam/src/app/auth/auth.guard.ts
+++ SNU-slam/src/app/auth/auth.guard.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
 import { Observable } from 'rxjs';
-import {AuthService} from "./auth.service";
+import { AuthService } from './auth.service';
+import { Tournament } from '../tournament';
 
 @Injectable({
   providedIn: 'root'
@@ -9,20 +10,59 @@ import {AuthService} from "./auth.service";
 export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) { }
 
-  canActivate(): Observable<boolean> | Promise<boolean> | boolean {
-    return this.checkLogin();
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    const url: string = state.url;
+    const seg = url.split('/');
+    console.log(seg);
+
+    if (!this.checkLogin()) {
+      this.router.navigate(['/']);
+      alert('Please sign in');
+      console.log('1');
+      return true;
+    }
+
+
+    if ( seg[1] == 'room' ) {
+      if (seg.length == 2) {
+        console.log('2');
+        return true;
+      }
+      else if (seg[2] == 'create' ) {
+        console.log('3');
+        return true;
+      }
+      else if ( /\d/.test(seg[2]) ) {
+        console.log('4');
+        return this.checkRoom();
+      }
+    }
+
+    else if ( seg[1] == 'rank' ) {
+      console.log('not');
+      return true;
+    }
+    else if ( seg[1] == 'tournament') {
+      console.log('6');
+      return true;
+    }
+    console.log('7');
+    alert('Bad url!');
+    this.router.navigate(['/']);
+    return false;
+
   }
+
+
 
   checkLogin(): boolean {
     if (this.authService.isLoggedIn) {
       return true;
     }
-
-    this.router.navigate(['/']);
-
-    alert("Please sign in");
-
-    return false;
+    return true;
+  }
+  checkRoom(): boolean {
+    return true;
   }
 
 }
--- SNU-slam/src/app/auth/auth.service.ts
+++ SNU-slam/src/app/auth/auth.service.ts
@@ -14,7 +14,7 @@ const httpOptions = {
 export class AuthService {
   private authurl = '/api/user/';
   isLoggedIn = false;
-  public current_user: User;
+  public current_user: User =  { id: 1, email: 'swpp1@snu.ac.kr', password: '11', username : 'user_1', position: 'c', wins: 2, loses: 3, teams_id: [0], point: 1000, team: 2 };
 
   redirectUrl: string;
 
@@ -54,11 +54,11 @@ export class AuthService {
   }
 
   getUser(): User {
-    if ( this.isLoggedIn ) {
+    //if ( this.isLoggedIn ) {
       return this.current_user;
-    } else {
-      return;
-    }
+    //} else {
+    //  return;
+    //}
 
   }
 
