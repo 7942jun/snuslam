8710a6d71af28e4bcdb624b9b6d54f82bb166da2
kimwoojung <kimwj317@naver.com>
2018-12-15 17:51:11 +0900

    tournament auth guard initial version

--- SNU-slam/src/app/auth/auth.guard.ts
+++ SNU-slam/src/app/auth/auth.guard.ts
@@ -3,12 +3,18 @@ import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '
 import { Observable } from 'rxjs';
 import { UserService } from '../services/user.service';
 import { Tournament } from '../tournament';
+import { TournamentService } from '../tournament/tournament.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  constructor(private userService: UserService, private router: Router) { }
+  constructor(
+    private userService: UserService, 
+    private router: Router,
+    private tournamentService: TournamentService
+  ) { }
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     const url: string = state.url;
@@ -43,18 +49,18 @@ export class AuthGuard implements CanActivate {
       if (seg.length == 2) {
         return true;
       }
-      else if (seg[2] == 'create') {
+      else if (seg[2] == 'create' && seg.length==3) {
         return true;
       }
-      else if (seg[2] == 'ongoing' ) {
+      else if (seg[2] == 'ongoing' && seg.length==4) {
         if (/^\d+$/.test(seg[3])) {
-          return this.checkongoing(Number(seg[3]));
+          this.checkTournamentOngoing(Number(seg[3]));
 
         }
       }
-      else if ( seg[2] == 'participate' ) {
+      else if ( seg[2] == 'participate' && seg.length==4) {
         if ( /^\d+$/.test(seg[3])) {
-          return this.checktorunament(Number(seg[3]));
+          this.checkTournamentParticipate(Number(seg[3]));
         }
       }
     }
@@ -76,11 +82,22 @@ export class AuthGuard implements CanActivate {
     //if (this.authService.isInRoom()) {}
     return true;
   }
-  checktorunament(id: number): boolean {
-    return true;
+  checkTournamentOngoing(id: number){
+    
+    this.tournamentService.getTournamentById(id)
+      .subscribe(tournament => {
+        if(tournament.id == id && (tournament.state == 3 || tournament.state == 4)){
+          return true;
+        }
+      });
   }
-  checkongoing(id: number): boolean {
-    return true;
+  checkTournamentParticipate(id: number){
+    this.tournamentService.getTournamentById(id)
+      .subscribe(tournament => {
+        if(tournament.id == id && tournament.state == 2){
+          return true;
+        }
+      });
   }
 
 }
