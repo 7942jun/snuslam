3214597ab7ce2843debc11fdfbf23eafa8a0a570
kimwoojung <kimwj317@naver.com>
2018-12-16 15:56:38 +0900

    local test

--- SNU-slam/src/environments/environment.ts
+++ SNU-slam/src/environments/environment.ts
@@ -3,8 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  API_URL: 'ec2-54-180-99-47.ap-northeast-2.compute.amazonaws.com',
+  production: false,
+  API_URL: '127.0.0.1:8000'
+  //production: true,
+  //API_URL: 'ec2-54-180-99-47.ap-northeast-2.compute.amazonaws.com',
+  
 };
 
 /*
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -87,7 +87,8 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('ec2-54-180-99-47.ap-northeast-2.compute.amazonaws.com', 6379)],
+            #"hosts": [('ec2-54-180-99-47.ap-northeast-2.compute.amazonaws.com', 6379)],
+            "hosts": [('127.0.0.1', 6379)],
         },
     },
 }
